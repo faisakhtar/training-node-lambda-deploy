c9a1c68b21e026110a63571857fe839cbf82498a
faisakhtar <faisakhtar@gmail.com>
2022-09-26 13:15:23 +0100

    Update output message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda Faisal!'),
+        body: JSON.stringify('Hello from Lambda Faisal, how are you today?'),
     };
     return response;
 };
