e4edd5f8ea3a0346fa32442c03b920490b836ffc
faisakhtar <faisakhtar@gmail.com>
2022-09-26 11:39:56 +0100

    Update index to include lambda syntax

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Just Testing")
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda Faisal!'),
+    };
+    return response;
+};
