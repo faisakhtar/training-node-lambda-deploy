40bcb91db07d2493a1292eed27cd6840f6d29a56
faisakhtar <faisakhtar@gmail.com>
2022-09-26 14:11:30 +0100

    Add scan query to dynamodb

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda Faisal, how are you today?'),
-    };
-    return response;
-};
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName : 'animals',
+}
+
+async function getItem(){
+  try {
+    const data = await docClient.scan(params).promise()
+    return data
+  } catch (err) {
+    return err
+  }
+}
+
+exports.handler = async (event, context) => {
+  try {
+    const data = await getItem()
+    return { body: JSON.stringify(data) }
+  } catch (err) {
+    return { error: err }
+  }
+}
