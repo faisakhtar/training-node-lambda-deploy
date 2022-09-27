6ed853604befb6163c45744969c48ac29f815fb1
faisakhtar <faisakhtar@gmail.com>
2022-09-27 09:36:18 +0100

    Add response data headers

--- index.js
+++ index.js
@@ -25,7 +25,15 @@ exports.handler = async (event, context) => {
   var queryId = event.pathParameters.id;
   try {
     const data = await getItem(queryId)
-    return { body: JSON.stringify(data) }
+    return { 
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        body: JSON.stringify(data) 
+    }
   } catch (err) {
     return { error: err }
   }
